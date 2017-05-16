531a84a3b96fd652ceeebcf1a049010d3e6c8cea
elad248 <elad248@gmail.com>
2017-05-16 21:55:35 +0300

    Update Validation.java

--- client/app/src/main/java/com/ahlan/ahlanapp/Validation.java
+++ client/app/src/main/java/com/ahlan/ahlanapp/Validation.java
@@ -2,14 +2,6 @@ package com.ahlan.ahlanapp;
 
 import java.util.regex.Pattern;
 
-/**
- * Created by Administrator on 2/12/2017.
- */
-
-/*
-class with static methods of the use of the others classes
- */
-
 public class Validation
 {
     public static boolean isNameValid(String name) {
@@ -20,5 +12,10 @@ public class Validation
     public static boolean isPasswordValid(String password) {
         Pattern p = Pattern.compile("[^a-zA-Z0-9|_]");
         return !p.matcher(password).find() && password.length() >= 4;
+    }
+
+    public static boolean isPhoneValid(String phone) {
+        Pattern p = Pattern.compile("[^0-9]");
+        return !p.matcher(phone).find();
     }
 }
